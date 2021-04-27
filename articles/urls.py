4706372e70077257bdb496831a24248e14de41a2
omacha502 <patrickoluwafor@gmail.com>
2021-04-27 14:40:08 +0100

    initial commit

--- articles/urls.py
+++ articles/urls.py
@@ -6,11 +6,11 @@ from .views import ArticleListView, ArticleUpdateView, ArticleDetailView, Articl
 urlpatterns = [
     path('<int:pk>/edit/',
          ArticleUpdateView.as_view(), name='article_edit'),
-    path('<int:pk>/',
+    path('post/<int:pk>/',
          ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/delete/',
          ArticleDeleteView.as_view(), name='article_delete'),
     path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('', ArticleListView.as_view(), name='article_list'),
-    path('new_comment', AddCommentView.as_view(), name='add_comment'),
+    path('new_comment/', AddCommentView.as_view(), name='add_comment'),
 ]
