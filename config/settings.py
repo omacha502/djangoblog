2024198d483984276b6f491c7a94c136d7b5d37b
omacha502 <patrickoluwafor@gmail.com>
2021-04-29 01:32:32 +0100

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'articles',
 ]
 
-TIME_ZONE = 'Africa/Lagos'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -111,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Lagos'
 
 USE_I18N = True
 
